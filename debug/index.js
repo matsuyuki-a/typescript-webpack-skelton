c564a1f8b731ac9b2ee445949b6363cb9004e1aa
@rmn_e <matsuyuki.eca@gmail.com>
2017-11-18 23:35:18 +0900

    install http-server

--- debug/index.js
+++ debug/index.js
@@ -73,9 +73,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__style_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__style_css__);
 
 
-
-console.log(1);
-
 function component() {
     var element = document.createElement('div');
     element.classList.add('hello');
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import "./style.css";
 
-
-console.log(1);
-
 function component() {
     var element = document.createElement('div');
     element.classList.add('hello');

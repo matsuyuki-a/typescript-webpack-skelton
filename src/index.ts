eaa3dea7d0c46045042f3abb5db748a320be5602
Matsuyuki/rmn. (@rmn_e) <matsuyuki.eca@gmail.com>
2018-06-02 20:35:06 +0900

    chore(tsconfig): strict: true化

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import "./style/style.scss";
 function component() {
     const element = document.createElement('p');
     element.classList.add('hello');
-    element.textContent="Hello, World!";
+    element.textContent = "Hello, World!";
     return element;
-  }
+}
 
 window.addEventListener("load", () => {
-    document.getElementById("container").appendChild(component());
+    const container = document.getElementById("container");
+    if (container) {
+        container.appendChild(component());
+    }
 });
